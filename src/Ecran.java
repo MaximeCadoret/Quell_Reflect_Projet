60e38bb59f1645147a2c23e704f238cbe8b3711c
MaximeCadoret <maxime.cadoret@supelec.fr>
2015-05-07 21:10:18 +0200

    début classe écran

--- src/Ecran.java
+++ src/Ecran.java
@@ -1,4 +1,63 @@
-public interface Ecran {
+import javax.swing.*;
 
-	Plateau plateau = null;
+import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.io.IOException;
+
+public class Ecran extends JPanel {
+
+	// Attributs
+
+	public Plateau grille;
+	public GridBagLayout gbl;
+	public GridBagConstraints gbc;
+
+	// Constructeurs
+
+	public Ecran(int num) throws IOException {
+		this.gbl = new GridBagLayout();
+		this.gbc = new GridBagConstraints();
+		this.gbc.fill = GridBagConstraints.BOTH;
+		this.setLayout(this.gbl);
+		this.grille = new Plateau(num);
+
+		dessin();
+	}
+
+	// Dessiner
+
+	public void dessin() {
+
+		int a = 0;
+		ImageIcon image;
+		JLabel fenetre;
+
+		removeAll();
+
+		for (int i = 0; i < this.grille.plateau.length; i++) {
+			for (int j = 0; j < this.grille.plateau[i].length; j++) {
+
+				this.gbc.gridx = i;
+				this.gbc.gridy = j;
+
+				if (this.grille.objetMouvant.length > 0
+						&& (this.grille.objetMouvant[a].x == i && this.grille.objetMouvant[a].y == j)) {
+					image = this.grille.objetMouvant[a].getImage();
+				} else {
+
+					image = this.grille.plateau[i][j].getImage();
+
+					fenetre = new JLabel(image);
+
+					this.gbl.setConstraints(fenetre, this.gbc);
+					add(fenetre);
+
+					revalidate();
+					repaint();
+				}
+			}
+		}
+
+	}
 }
